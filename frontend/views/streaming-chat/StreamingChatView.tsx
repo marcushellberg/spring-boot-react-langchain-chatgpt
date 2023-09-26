d792542cc28c7a7b959db2daca854efc1532fb00
Marcus Hellberg <marcus@vaadin.com>
2023-09-26 09:07:42 -0700

    refactor streaming code for legibility

--- frontend/views/streaming-chat/StreamingChatView.tsx
+++ frontend/views/streaming-chat/StreamingChatView.tsx
@@ -6,25 +6,36 @@ import {MessageInput} from "@hilla/react-components/MessageInput";
 export default function StreamingChatView() {
     const [messages, setMessages] = useState<MessageListItem[]>([]);
 
+    function addMessage(message: MessageListItem) {
+        setMessages(messages => [...messages, message]);
+    }
+    function appendToLastMessage(chunk: string) {
+        setMessages(messages => {
+            const lastMessage = messages[messages.length - 1];
+            return [...messages.slice(0, -1), {
+                text: lastMessage.text + chunk,
+                userName: lastMessage.userName
+            }];
+        });
+    }
+
     async function sendMessage(message: string) {
-        setMessages(messages => [...messages, {
+        addMessage({
             text: message,
             userName: 'You'
-        }]);
+        });
 
         let first = true;
         ChatService.chatStream(message).onNext(chunk => {
             if (first) {
-                setMessages(messages => [...messages, {
+                addMessage({
                     text: chunk,
                     userName: 'Assistant'
-                }]);
+                });
+
                 first = false;
             } else {
-                setMessages(messages => [...messages.slice(0, -1), {
-                    text: messages[messages.length - 1].text + chunk,
-                    userName: 'Assistant'
-                }]);
+                appendToLastMessage(chunk);
             }
         });
     }
